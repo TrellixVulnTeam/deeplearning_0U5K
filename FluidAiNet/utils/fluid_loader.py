0b9fdeafe59b8683e947f86ba4b94f4b4409c4b6
changxincheng <chengcx1019@gmail.com>
2018-05-06 17:14:39 +0800

    pointcloud preprocess for fluid

--- FluidAiNet/utils/fluid_loader.py
+++ FluidAiNet/utils/fluid_loader.py
@@ -1,3 +1,4 @@
+
 #!/usr/bin/env python
 # encoding: utf-8
 
@@ -7,6 +8,142 @@
 @file: fluid_loader.py
 @time: 2018/5/4 15:04
 """
+from __future__ import print_function
+from __future__ import division
+
+import time
+import operator
+import os
+
+from nose.tools import assert_equal
+import numpy as np
+import pandas as pd
+
+BASE_DIR = '/data/datasets/simulation_data'
+DATA_DIR = os.path.join(BASE_DIR, 'water/0')
+
+if not os.path.exists(DATA_DIR):
+    os.mkdir(DATA_DIR)
+
+def get_data_files():
+    filenames = []
+    # TODO multi folders for different frames
+    allfiles = os.listdir(DATA_DIR)
+    for filename in allfiles:
+        if filename.endswith('.csv'):
+            filenames.append(filename)
+    return map(lambda x: os.path.join(DATA_DIR, x), filenames)
+
+def create_train_files(max_num):
+    TRAIN_FILES = []
+    files_map = get_data_files()
+    i = 0
+    max_count = 2
+    for item in files_map:
+        if i == max_count:
+            break
+        TRAIN_FILES.append(item)
+        i = i + 1
+
+    assert_equal(len(TRAIN_FILES), max_count)
+    return TRAIN_FILES
+
+def convert_str_float(frame_particles):
+    fps = pd.DataFrame(frame_particles[1:], columns=frame_particles[0])
+    fps = fps[fps.columns[:-1]]
+    for col in fps.columns:
+        #if col == 'isFluidSolid':
+        fps[col] = fps[col].astype(float)
+    return fps
+
+def laod_csv(filename):
+    frame_particles = np.loadtxt(
+            filename, dtype=np.str, delimiter=",")
+    return convert_str_float(frame_particles)
+
+def load_data_file(filename):
+    suffix = filename.split('/')[-1].split('.')[-1]
+    print(suffix)
+    if suffix == 'csv':
+        return laod_csv(filename)
+
+def load_data_label(filename):
+    particles = load_data_file(filename)
+    cols = particles.columns
+    data_cols = operator.add(list(cols[0:6]), list(cols[7:9]))
+    label_cols = cols[15:18]
+    data = particles[data_cols].values
+    label = particles[label_cols].values
+    return data, label
+
+def shuffle_data(data, labels):
+    """ Shuffle data and labels.
+        Input:
+          data: B,N,... numpy array
+          label: B,N... numpy array
+        Return:
+          shuffled data, label and shuffle indices
+    """
+    idx = np.arange(labels.shape[0])
+    np.random.shuffle(idx)
+    return data[idx, ...], labels[idx, ...], idx
+
+def concat_data_label(train_files, max_points, dimention_data, dimention_label):
+    """
+    intercept max_points
+    """
+    TRAIN_FILES = train_files
+    train_file_idxs = np.arange(0, len(TRAIN_FILES))
+    np.random.shuffle(train_file_idxs)
+    def get_array(shape):
+        return np.empty(shape=shape)
+    FRAMES_NUM = len(TRAIN_FILES)
+    MAX_POINTS = max_points
+    DIMENTION_DATA = dimention_data
+    DIMENTION_LABEL = dimention_label
+    data_shape = (FRAMES_NUM, MAX_POINTS, DIMENTION_DATA)
+    """
+    Different from the classification task, our lable is for every particle, we record label with (frame, particle index \
+    , three-dimentions accelaration)(BxNx3)
+    """
+    label_shape = (FRAMES_NUM, MAX_POINTS, DIMENTION_LABEL)
+    current_data = get_array(data_shape)
+    current_label = get_array(label_shape)
+    start = time.clock()
+    for fn in range(len(TRAIN_FILES)):
+        current_data_single, current_label_single = load_data_label(TRAIN_FILES[train_file_idxs[fn]])
+        current_data[fn] = current_data_single.values[:MAX_POINTS, :]
+        current_label[fn]= current_label_single.values[:MAX_POINTS, :]
+    running = time.clock() - start
+    print("runtime: %s" % str(running))
+    return current_data, current_label
+
+def concat_data_label_all(train_files, dimention_data, dimention_label):
+    """
+    use all data in a file
+    """
+    TRAIN_FILES = train_files
+    train_file_idxs = np.arange(0, len(TRAIN_FILES))
+    np.random.shuffle(train_file_idxs)
+
+    DIMENTION_DATA = dimention_data
+    DIMENTION_LABEL = dimention_label
+    """
+    Different from the classification task, our lable is for every particle, we record label with (frame, particle index \
+    , three-dimentions accelaration)(BxNx3)
+    """
+    current_data = []
+    current_label = []
+    start = time.clock()
+    for fn in range(len(TRAIN_FILES)):
+        current_data_single, current_label_single = load_data_label(TRAIN_FILES[train_file_idxs[fn]])
+        current_data.append(current_data_single)
+        current_label.append(current_label_single)
+    running = time.clock() - start
+    print("runtime: %s" % str(running))
+    return current_data, current_label
 
 if __name__ == '__main__':
-    pass
+    BATCH_SIZE = 2
+    TRAIN_FILES = create_train_files(2)
+    print(TRAIN_FILES)
---
+++ FluidAiNet/utils/preprocess.py
@@ -0,0 +1,166 @@
+import os
+import multiprocessing
+import numpy as np
+from nose.tools import assert_equal
+
+from config import cfg
+
+data_dir = 'velodyne'
+
+
+"""
+screen size (-8, -8, -8) (8, 8, 12)
+spacing_r 0.1f # diameter 
+amoothRadius_h  2*spacing_r # so i decide the voxel_size 4*spacing_r
+particle size - radius
+x,y,z(21,21,26) (-1, -1, -1) (1, 1, 1.5)
+theoreticlly, 
+"""
+
+def fluid_process_pointcloud(point_cloud, cls=cfg.DETECT_OBJ):
+    if cls == 'Fluid':
+        print('Fluid')
+        scene_size = np.array([16, 16, 20], dtype=np.float32)
+        voxel_size = np.array([0.4, 0.4, 0.4], dtype=np.float32)
+        grid_size = np.array([40, 40, 50], dtype=np.int64)
+        lidar_coord = np.array([8, 8, 8], dtype=np.float32)
+        max_point_number = 64
+        # return
+    # FIXME  ccx AWESOME
+    shifted_coord = point_cloud[:, :3] + lidar_coord
+    # reverse the point cloud coordinate (X, Y, Z) -> (Z, Y, X)
+    voxel_index = np.floor(
+        shifted_coord[:, ::-1] / voxel_size).astype(np.int)  # int lower than num
+
+    print(voxel_index.shape)
+    bound_x = np.logical_and(
+        voxel_index[:, 2] >= 0, voxel_index[:, 2] < grid_size[2])
+    bound_y = np.logical_and(
+        voxel_index[:, 1] >= 0, voxel_index[:, 1] < grid_size[1])
+    bound_z = np.logical_and(
+        voxel_index[:, 0] >= 0, voxel_index[:, 0] < grid_size[0])
+
+    bound_box = np.logical_and(np.logical_and(bound_x, bound_y), bound_z)
+
+    point_cloud = point_cloud[bound_box]
+    voxel_index = voxel_index[bound_box]
+
+    # [K, 3] coordinate buffer as described in the paper
+    coordinate_buffer = np.unique(voxel_index, axis=0)
+
+    K = len(coordinate_buffer)  # K record the number of voxels
+    T = max_point_number
+    print(K)
+    assert_equal(T, 64)
+    # [K, 1] store number of points in each voxel grid
+    number_buffer = np.zeros(shape=(K), dtype=np.int64)
+
+    # [K, T, 7] feature buffer as described in the paper
+    feature_buffer = np.zeros(shape=(K, T, 11), dtype=np.float32) # position, velocity, isFluid, index, relative position
+
+    # build a reverse index for coordinate buffer
+    index_buffer = {}
+    for i in range(K):
+        # mark the voxel,the position tuple of voxel as list(index_buffer)'s key
+        index_buffer[tuple(coordinate_buffer[i])] = i
+
+    for voxel, point in zip(voxel_index, point_cloud):
+        index = index_buffer[tuple(voxel)]
+        number = number_buffer[index]
+        if number < T:
+            feature_buffer[index, number, :4] = point
+            number_buffer[index] += 1
+
+    feature_buffer[:, :, -3:] = feature_buffer[:, :, :3] - \
+                                feature_buffer[:, :, :3].sum(axis=1, keepdims=True) / number_buffer.reshape(K, 1, 1)
+
+    voxel_dict = {'feature_buffer': feature_buffer,  # (K, T, 7)
+                  'coordinate_buffer': coordinate_buffer,  # (K, 3)
+                  'number_buffer': number_buffer}  # (K,)
+    return voxel_dict
+
+def process_pointcloud(point_cloud, cls=cfg.DETECT_OBJ):
+    # Input:
+    #   (N, 4)
+    # Output:
+    #   voxel_dict
+    if cls == 'Car':
+        scene_size = np.array([4, 80, 70.4], dtype=np.float32)
+        voxel_size = np.array([0.4, 0.2, 0.2], dtype=np.float32)
+        grid_size = np.array([10, 400, 352], dtype=np.int64)
+        lidar_coord = np.array([0, 40, 3], dtype=np.float32)
+        max_point_number = 35
+    elif cls == 'Fluid':
+        print('Fluid')
+        scene_size = np.array([16, 16, 20], dtype=np.float32)
+        voxel_size = np.array([0.4, 0.4, 0.4], dtype=np.float32)
+        grid_size = np.array([10, 400, 352], dtype=np.int64)
+        lidar_coord = np.array([8, 8, 8], dtype=np.float32)
+        max_point_number = 64
+    else:
+        scene_size = np.array([4, 40, 48], dtype=np.float32)
+        voxel_size = np.array([0.4, 0.2, 0.2], dtype=np.float32)
+        grid_size = np.array([10, 200, 240], dtype=np.int64)
+        lidar_coord = np.array([0, 20, 3], dtype=np.float32)# fix coordinate to positive(z,y,x),which is the max num of minus
+        max_point_number = 45
+
+        np.random.shuffle(point_cloud)
+    # FIXME  ccx AWESOME
+    shifted_coord = point_cloud[:, :3] + lidar_coord
+    # reverse the point cloud coordinate (X, Y, Z) -> (Z, Y, X)
+    voxel_index = np.floor(
+        shifted_coord[:, ::-1] / voxel_size).astype(np.int) # int lower than num
+
+    bound_x = np.logical_and(
+        voxel_index[:, 2] >= 0, voxel_index[:, 2] < grid_size[2])
+    bound_y = np.logical_and(
+        voxel_index[:, 1] >= 0, voxel_index[:, 1] < grid_size[1])
+    bound_z = np.logical_and(
+        voxel_index[:, 0] >= 0, voxel_index[:, 0] < grid_size[0])
+
+    bound_box = np.logical_and(np.logical_and(bound_x, bound_y), bound_z)
+
+    point_cloud = point_cloud[bound_box]
+    voxel_index = voxel_index[bound_box]
+
+    # [K, 3] coordinate buffer as described in the paper
+    coordinate_buffer = np.unique(voxel_index, axis=0)
+
+    K = len(coordinate_buffer) # K record the number of voxels
+    T = max_point_number # TODO ccx set T reasonable?
+
+    # [K, 1] store number of points in each voxel grid
+    number_buffer = np.zeros(shape=(K), dtype=np.int64)
+
+    # [K, T, 7] feature buffer as described in the paper
+    feature_buffer = np.zeros(shape=(K, T, 7), dtype=np.float32)
+
+    # build a reverse index for coordinate buffer
+    index_buffer = {}
+    for i in range(K):
+        # mark the voxel,the position tuple of voxel as list(index_buffer)'s key
+        index_buffer[tuple(coordinate_buffer[i])] = i
+
+    for voxel, point in zip(voxel_index, point_cloud):
+        index = index_buffer[tuple(voxel)]
+        number = number_buffer[index]
+        if number < T:
+            feature_buffer[index, number, :4] = point
+            number_buffer[index] += 1
+
+    feature_buffer[:, :, -3:] = feature_buffer[:, :, :3] - \
+        feature_buffer[:, :, :3].sum(axis=1, keepdims=True)/number_buffer.reshape(K, 1, 1)
+
+    voxel_dict = {'feature_buffer': feature_buffer, # (K, T, 7)
+                  'coordinate_buffer': coordinate_buffer, # (K, 3)
+                  'number_buffer': number_buffer} # (K,)
+    return voxel_dict
+
+if __name__ == "__main__":
+    import fluid_loader
+
+    BATCH_SIZE = 2
+    TRAIN_FILES = fluid_loader.create_train_files(BATCH_SIZE)
+    print(TRAIN_FILES)
+    pointcloud, _ = fluid_loader.concat_data_label_all(TRAIN_FILES, 8, 3)
+    voxel_index = fluid_process_pointcloud(pointcloud[0])
